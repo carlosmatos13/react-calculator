f499b7d0c6de38c9cd24a7bf6577ae5db17fca9f
LeMatos <carlosmatos13@gmail.com>
2020-06-02 11:46:25 -0400

    Fix test description

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer';
 import App from './App';
 
 
-describe('Calculator component', () => {
+describe('renders correctly', () => {
     it('matches the snapshot',() => {
         const tree = renderer.create(<App />).toJSON();
         expect(tree).toMatchSnapshot();
